1b5ad987ae60ac81c11f05eb0d93c8755d02a1ee
photino <panzan89@gmail.com>
2017-07-01 22:43:38 +0800

    Update version 0.0.5

---
+++ dist/icons/weather.js
@@ -0,0 +1,14 @@
+/**
+ * Weather Icons (Built with IcoMoon)
+ *  - Font Awesome: Dave Gandy, GPL / CC BY 4.0
+ *  - Octicons: Github, MIT
+ */
+
+(function ($) {
+  'use strict';
+
+  schema.icons = $.extend(schema.icons, {
+
+  });
+
+})(jQuery);
